2b6b5a916ae4912ef8d2730171ed2bcf2d0d2edb
Sandra <Sandra.A3@o2.pl>
2021-04-18 09:42:23 +0200

    CS50Web Project 1 wiki: work in progress

--- CS50Web/Project 1/wiki/wiki/encyclopedia/urls.py
+++ CS50Web/Project 1/wiki/wiki/encyclopedia/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("wiki/<str:entry>", views.entry, name="entry"),
+    path("create", views.create, name="create"),
+    path("search", views.search, name="search")
 ]
--- CS50Web/Project 1/wiki/wiki/encyclopedia/views.py
+++ CS50Web/Project 1/wiki/wiki/encyclopedia/views.py
@@ -1,10 +1,43 @@
 from django.shortcuts import render
 
 from . import util
+from django import forms
 
+class CreateForm(forms.Form):
+    title = forms.CharField(label="Title")
+    content = forms.CharField(label="Content")
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
+
+def entry(request, entry):
+    display = util.get_entry(entry)
+    if display == None:
+        return ("Error")
+    return render(request, "encyclopedia/entry.html", {
+        "entry": display
+    })
+
+def create(request):
+    if request.method == "POST":
+        form = CreateForm(request.POST)
+        if util.get_entry(form[title]) != None:
+            return ("Error")
+        util.save_entry(form["title"], form["content"])
+    else:
+        form = CreateForm()
+        return render(request, "encyclopedia/create.html", {
+            "form": form
+            })
+
+def search(request):
+    display = util.get_entry(form.request.POST["p"])
+    if display == None:
+        return ("Error")
+    return render(request, "encyclopedia/entry.html", {
+        "entry": display
+    })
+    
 
