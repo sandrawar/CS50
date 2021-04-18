d3ad417111beb4c025479a8c8aad910afc1ab6a8
Sandra <Sandra.A3@o2.pl>
2021-04-18 16:32:48 +0200

    CS50AI Project 1 knights: final version

--- CS50AI/Project1/knights/knights/puzzle.py
+++ CS50AI/Project1/knights/knights/puzzle.py
@@ -12,26 +12,71 @@ CKnave = Symbol("C is a Knave")
 # Puzzle 0
 # A says "I am both a knight and a knave."
 knowledge0 = And(
-    AKnave,
-    Not(AKnight)
+    # A is knight or knave and only one of them
+    Biconditional(
+        AKnight, 
+        Not(AKnave)),
+    # If A says true (is a knight), and only then, A is both - knight and knave
+    Biconditional(
+        AKnight, 
+        And(
+            AKnight,
+            AKnave))
 )
 
 # Puzzle 1
 # A says "We are both knaves."
 # B says nothing.
 knowledge1 = And(
-    Or(And(AKnight, AKnave, BKnave), And(AKnave, Not(And(AKnave, BKnave)))) 
+    # A is knight or knave and only one of them
+    Biconditional(
+        AKnight, 
+        Not(AKnave)),
+    # B is knight or knave and only one of them
+    Biconditional(
+        BKnight, 
+        Not(BKnave)),
+    # If A says true (is a knight), and only then, A and B are both knaves
+    Biconditional(
+        AKnight, 
+        And(
+            AKnave, 
+            BKnave)) 
 )
 
 # Puzzle 2
 # A says "We are the same kind."
 # B says "We are of different kinds."
 knowledge2 = And(
-    Biconditional(AKnight, Or(And(AKnight, BKnight), And(AKnave, BKnave))),
-    Biconditional(BKnight, Or(And(AKnight, BKnave), And(AKnave, BKnight))),
-    Biconditional(AKnave, Not(Or(And(AKnight, BKnight), And(AKnave, BKnave)))),
-    Biconditional(BKnave, Not(Or(And(AKnight, BKnave), And(AKnave, BKnight))))
-)
+    # A is knight or knave and only one of them
+    Biconditional(
+        AKnight, 
+        Not(AKnave)),
+    # B is knight or knave and only one of them
+    Biconditional(
+        BKnight, 
+        Not(BKnave)),
+    # If A says true (is a knight), and only then, A and B are the same kind (are both knaves or are both knights)
+    Biconditional(
+        AKnight, 
+        Or(
+            And(
+                AKnight, 
+                BKnight), 
+            And(
+                AKnave, 
+                BKnave))),
+    # If B says true (is a knight), and only then, A and B are different kinds
+    Biconditional(
+        BKnight, 
+        Or(
+            And(
+                AKnight, 
+                BKnave), 
+            And(
+                AKnave, 
+                BKnight))),
+    )
 
 # Puzzle 3
 # A says either "I am a knight." or "I am a knave.", but you don't know which.
@@ -39,14 +84,40 @@ knowledge2 = And(
 # B says "C is a knave."
 # C says "A is a knight."
 knowledge3 = And(
-    Biconditional(AKnight, AKnight),
-    Biconditional(AKnave, AKnight),
-    Biconditional(BKnight, Implication(AKnight, AKnave)),
-    Biconditional(BKnave, Not(Implication(AKnight, AKnave))),
-    Biconditional(BKnight, CKnave),
-    Biconditional(BKnave, Not(CKnave)),
-    Biconditional(CKnight, AKnight),
-    Biconditional(CKnave, Not(AKnight))
+    # A is knight or knave and only one of them
+    Biconditional(
+        AKnight, 
+        Not(AKnave)),
+    # B is knight or knave and only one of them
+    Biconditional(
+        BKnight, 
+        Not(BKnave)),
+    # C is knight or knave and only one of them
+    Biconditional(
+        CKnight, 
+        Not(CKnave)),
+    # If A says true (is a knight), and only then, A is either knight or knave
+    Biconditional(
+        AKnight, 
+        Biconditional(
+            AKnight,
+            Not(AKnave))),
+    # If B says true (is a knight), and only then, we can deduce from what A said, that B is a Knave
+    Biconditional(
+        BKnight, 
+        Implication(
+            Biconditional(
+                AKnight,
+                Not(AKnave)), 
+            AKnave)),
+    # If B says true (is a knight), and only then, C is a knave
+    Biconditional(
+        BKnight, 
+        CKnave),
+    # If C says true (is a knight), and only then, A is a knight
+    Biconditional(
+        CKnight, 
+        AKnight),
 )
 
 
