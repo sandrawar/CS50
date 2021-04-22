d049dfab7fb5935a738e3796c1b7340e14a5a90f
Sandra <Sandra.A3@o2.pl>
2021-04-22 12:17:49 +0200

    CS50Web Project 2 commerce: create new listing and active listing views added

--- CS50Web/Project 2/commerce/auctions/models.py
+++ CS50Web/Project 2/commerce/auctions/models.py
@@ -2,5 +2,22 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
+class Listing(models.Model):
+    title = models.TextField(max_length=64)
+    description = models.TextField()
+    bid = models.DecimalField(max_digits=5, decimal_places=2)
+    photo = models.URLField(blank=True)
+    class Category(models.TextChoices):
+        FASHION = "Fashion"
+        TOYS = "Toys"
+        ELECTRONICS = "Electronics"
+        HOME = "Home"
+        BOOKS = "Books"
+    #category = models.TextField(choices=Category.choices)
+
+class Bid(models.Model):
+    product = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name="product")
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    
 class User(AbstractUser):
-    pass
+    listings = models.ManyToManyField(Listing, blank=True, related_name="owner")
--- CS50Web/Project 2/commerce/auctions/views.py
+++ CS50Web/Project 2/commerce/auctions/views.py
@@ -3,8 +3,9 @@ from django.db import IntegrityError
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
+from django.contrib.auth.decorators import login_required
 
-from .models import User
+from .models import User, Listing, Bid
 
 
 def index(request):
@@ -61,3 +62,41 @@ def register(request):
         return HttpResponseRedirect(reverse("index"))
     else:
         return render(request, "auctions/register.html")
+
+@login_required
+def create(request):
+    if request.method == "POST":
+        try:
+            title = request.POST["title"]
+            description = request.POST["description"]
+            bid = request.POST["bid"]
+            #category = request.POST["category"]
+        except IntegrityError:
+            return render(request, "auctions/create.html", {
+                "message": "Not all data provided."
+            })
+        try:
+            photo = request.POST["photo"]   
+        except:
+            pass
+        try:
+            listing = Listing(title=title, description=description, bid=bid, photo=photo, category=category)
+        except:
+            listing = Listing(title=title, description=description, bid=bid) #, category=category)
+        listing.save()
+        bid = Bid(product=listing, price=bid)
+        bid.save()
+        return HttpResponseRedirect(reverse("index"))
+    else:
+        return render(request, "auctions/create.html")
+
+def active(request):
+    return render(request, "auctions/active.html", {
+        "listings": Listing.objects.all()
+    })
+
+def listing(request, listing):
+    return render(request, "auctions/listing.html", {
+        "listing": Listing.objects.get(title=listing),
+        #"bid": Bid.objects.get(product=listing)
+    })
