935115dde2765e50e1dda91b231a3eb419fe4117
Sandra <Sandra.A3@o2.pl>
2021-04-27 07:28:08 +0200

    CS50Web Project 1 wiki: completed

--- CS50Web/Project 1/wiki/wiki/encyclopedia/views.py
+++ CS50Web/Project 1/wiki/wiki/encyclopedia/views.py
@@ -1,3 +1,4 @@
+import markdown2
 from django.shortcuts import redirect
 from django.shortcuts import render
 
@@ -7,7 +8,7 @@ from random import randrange
 
 class CreateForm(forms.Form):
     title = forms.CharField(label="Title", max_length=100)
-    content = forms.CharField(label="Content")
+    content = forms.CharField(label="Content", widget=forms.Textarea)
 
 class EditForm(forms.Form):
     title = forms.CharField(label="Title", max_length=100, widget=forms.HiddenInput())
@@ -21,9 +22,10 @@ def index(request):
 def entry(request, entry):
     display = util.get_entry(entry)
     if display == None:
-        raise Http404("Entry with this title does not exist.") 
+        raise Http404("Entry with this title does not exist.")
+    htmlDisplay = markdown2.markdown(display)
     return render(request, "encyclopedia/entry.html", {
-        "entry": display,
+        "entry": htmlDisplay,
         "title": entry
     })
 
@@ -66,7 +68,7 @@ def edit(request, entry):
             if util.get_entry(title) == None: 
                 raise Http404("Entry with that title does not exist.")
             util.save_entry(title, content)
-            return redirect('entry', entry= entry)
+            return redirect('entry', entry=entry)
 
     else:
         data = util.get_entry(entry)
