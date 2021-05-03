43b336fb4a11db990339aeed7aa2cc7ca79a25d1
Sandra <Sandra.A3@o2.pl>
2021-05-03 08:54:00 +0200

    CS50AI Project 5 traffic: work in progress

--- CS50AI/Project5/traffic/traffic.py
+++ CS50AI/Project5/traffic/traffic.py
@@ -58,8 +58,18 @@ def load_data(data_dir):
     be a list of integer labels, representing the categories for each of the
     corresponding `images`.
     """
-    raise NotImplementedError
-
+    #raise NotImplementedError
+    images = []
+    labels = []
+    path = os.path.normcase(data_dir)
+    for i in range(NUM_CATEGORIES):
+        imgDir = os.path.join(path, str(i))
+        for img in os.listdir(imgDir):
+            img_array = cv2.imread(os.path.join(imgDir, img))
+            img_array = cv2.resize(img_array, (IMG_WIDTH, IMG_HEIGHT), interpolation = cv2.INTER_AREA)
+            images.append(img_array)
+            labels.append(i)
+    return (images, labels)
 
 def get_model():
     """
@@ -67,8 +77,34 @@ def get_model():
     `input_shape` of the first layer is `(IMG_WIDTH, IMG_HEIGHT, 3)`.
     The output layer should have `NUM_CATEGORIES` units, one for each category.
     """
-    raise NotImplementedError
+    #raise NotImplementedError
+    # Create a convolutional neural network
+    model = tf.keras.models.Sequential([
 
+        # Convolutional layer. Learn 32 filters using a 3x3 kernel
+        tf.keras.layers.Conv2D(
+            32, (3, 3), activation="relu", input_shape=(IMG_WIDTH, IMG_HEIGHT, 3)
+        ),
+
+        # Max-pooling layer, using 2x2 pool size
+        tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
+
+        # Flatten units
+        tf.keras.layers.Flatten(),
+
+        # Add a hidden layer with dropout
+        tf.keras.layers.Dense(128, activation="relu"),
+        tf.keras.layers.Dropout(0.5),
+
+        # Add an output layer with output units for all 10 digits
+        tf.keras.layers.Dense(NUM_CATEGORIES, activation="softmax")
+    ])
+    model.compile(
+        optimizer="adam",
+        loss="categorical_crossentropy",
+        metrics=["accuracy"]
+    )
+    return model
 
 if __name__ == "__main__":
     main()
